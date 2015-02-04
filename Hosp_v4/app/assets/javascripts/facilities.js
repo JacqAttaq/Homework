6ac031000d345f0153e96e6ed97fa5bae12fbbbe
Jackie Tremblay <jackietremblayresponsibleadult@gmail.com>
2015-02-04 15:22:11 -0500

    facility validate submit

--- Hosp_v4/app/assets/javascripts/facilities.js
+++ Hosp_v4/app/assets/javascripts/facilities.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
 
+  $(".validate-test").blur(function(){
+    if($(".validate-test").val() != ""){
+      $(".submit-info").removeClass('disabled');
+    
+     }
+  });
   
-  
-})
+});
 
---
+++ Hosp_v4/app/assets/javascripts/meds.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+  $(".validate-test").blur(function(){
+    if($(".validate-test").val() != ""){
+      $(".submit-info").removeClass('disabled');
+    
+     }
+  });
+  
+});
