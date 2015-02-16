15066b2fdf1a3ca6f5d26cdbab446b6c99f90571
Jackie Tremblay <jackietremblayresponsibleadult@gmail.com>
2015-02-15 21:45:57 -0500

    assignment crud

--- iysm/app/controllers/assignments_controller.rb
+++ iysm/app/controllers/assignments_controller.rb
@@ -1,12 +1,19 @@
 class AssignmentsController < ApplicationController
+  before_action :set_assignment, only: [:show, :edit, :update, :destroy]
+  before_action :set_user
+
+
   def index
-    @user = User.find params[:user_id]
+    @assignments = Assignment.all
   end
 
   def new
+    @assignment = @user.assignments.new
   end
 
   def create
+    @assignment = @user.assignments.create assignment_params
+    redirect_to user_path(@user)
   end
 
   def edit
@@ -16,5 +23,22 @@ class AssignmentsController < ApplicationController
   end
 
   def destroy
+    @assignment.destroy
+    redirect_to user_assignments_path(@user)
+  end
+
+  private
+  def assignment_params
+    params.require(:assignment).permit(
+      :name,
+      :requirements)
+  end
+
+  def set_assignment
+    @assignment = Assignment.find params[:id]
+  end  
+
+  def set_user
+    @user = User.find params[:user_id]
   end
 end
