adf724f85b3dd30ea779751c27b12963d4127631
Jackie Tremblay <jackietremblayresponsibleadult@gmail.com>
2015-03-07 22:37:20 -0500

    extended pagination to facilites n patient

--- Hosp_v4/app/assets/javascripts/home.js
+++ Hosp_v4/app/assets/javascripts/home.js
@@ -6,5 +6,16 @@ $(document).ready(function(){
       $(".submit-info").removeClass('disabled')
     }
   });
+  
 
+  $('ul.property_list > li').hide();
+  $('li.section_1').show();
+  console.log("Code loaded");
+
+$("ul.link_list > li > a").click(function(){
+  $("ul.property_list> li").hide();
+  console.log("in the middle of the list");
+  var property_list = $(this).attr("med_num");
+  $("li.section_" + property_list).show();
+})
 })
--- Hosp_v4/app/assets/javascripts/meds.js
+++ Hosp_v4/app/assets/javascripts/meds.js
@@ -3,16 +3,7 @@ $(document).ready(function(){
   
 
 
-  $('ul.med_list >li').hide();
-  $('li.section_1').show();
-  console.log("Code loaded");
 
-$("ul.link_list > li > a").click(function(){
-  $("ul.med_list> li").hide();
-  console.log("in the middle of the list");
-  var med_list = $(this).attr("med_num");
-  $("li.section_" + med_list).show();
-})
 
 
 });
