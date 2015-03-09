3415e4410e81ed274125a4bea900e9e6629db4a1
Jackie Tremblay <jackietremblayresponsibleadult@gmail.com>
2015-03-09 12:02:39 -0400

    jed js

---
+++ jed_anagram/anagram.js
@@ -0,0 +1,20 @@
+var words = ["enlists", "google", "inlets", "banana"];
+
+var anagrams = {};
+
+for (var listen in words) {
+  var word = words[listen];
+  var sorted = sortWord(word);
+
+  if (anagrams[sorted]) != null) {
+      anagrams[sorted].push(word);      
+}
+  else {
+    anagrams[sorted]= [word];
+  }
+
+  for (var sorted in anagrams){
+    var words = anagrams[sorted];
+    var sep - 
+  }
+}
--- jed_anagram/anagram.rb
+++ jed_anagram/anagram.rb
@@ -1,11 +1,8 @@
 puts "Enter a word"
 
-words = ["enlists", "google", "inlets", "banana"]
+
 
 def anagram word
-word = gets.chomp.downcase
-  @words_hash = words.each_with_object(Hash.new[]) do |word,hash| 
-    hash[word.chars.sort] += [word]
-  end
-puts words_hash
+word = gets.chomp
+  @words_hash = words.group_by {|word| word.chars.sort}; @words_hash.default = ["enlists", "google", "inlets", "banana"]
 end
